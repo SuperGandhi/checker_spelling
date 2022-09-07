8227dd8d12dcc0f0f12aff2a4508cff7589c6d51
SuperGandhi <patrickborjabueno@hotmail.com>
2022-09-07 13:42:36 -0500

    Complete

---
+++ main.py
@@ -0,0 +1,35 @@
+from logging import root
+import tkinter
+from tkinter import *
+from textblob import TextBlob
+
+root=Tk()
+root.title('Spelling Checker')
+root.geometry('700x400')
+root.config(background='cyan')
+
+
+def check_spelling():
+    word=enter_text.get()
+    a=TextBlob(word)
+    right=str(a.correct())
+    
+    cs=Label(root,text='Correct text is :', font=('poppins', 20), bg='white', fg='grey')
+    cs.place(x=100,y=250)
+    spell.config(text=right)
+
+heading= Label(root, text='Spelling Checker', font=('Trebuchet MS', 30, 'bold'), bg='cyan', fg='black')
+heading.pack(pady=(50,0))
+
+enter_text=Entry(root, justify='center', width=30,font=('poppins', 25), bg='white', border=2)
+enter_text.pack(pady=10)
+enter_text.focus()
+
+button=Button(root,text='Check', font=('arial', 20, 'bold'), fg='white',bg='green', command=check_spelling)
+button.pack()
+
+spell=Label(root, font=('poppins', 20), bg='cyan', fg='black')
+spell.place(x=350, y=250)
+
+
+root.mainloop()
